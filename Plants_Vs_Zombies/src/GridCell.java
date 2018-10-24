1989c3cb663dfefec6bc2409c3997d4b078dfb1c
Leo <omarleopaz@hotmail.com>
2018-10-24 15:40:24 -0400

    Updated GridCell

--- Plants_Vs_Zombies/src/GridCell.java
+++ Plants_Vs_Zombies/src/GridCell.java
@@ -2,53 +2,73 @@ import java.util.*;
 
 public class GridCell {
 	
-	private LinkedList<Object> visitors;
-	/* Constructor for the grid class
-	 * **/
-	public GridCell () {
-		visitors = null;
-	}
+	private LinkedList<Zombie> zombies;
+	
+	private Plant plant;
 	
 	/* 
 	 * Constructor for the grid class that takes a
-	 * zombie or plant and placed it on the grid space
-	 * @param entity The entity that will start on the grid cell 
+	 * zombie or plant and placed it on the grid space 
 	 **/
-	public GridCell(Object entity) {
-		visitors.add(entity);
+	public GridCell () {
+		zombies = null;
+		plant = null;
 	}
 	
 	/* 
 	 * Getter method to see the visitors on the current grid cell
 	 * @return ArrayList of visitors on the grid 
 	 **/
-	public LinkedList<Object> getVisitors(){
-		return visitors;
+	public LinkedList<Object> getZombies(){
+		return zombies;
+	}
+	
+	/*
+	 * Retrieves the current plant on the grid cell
+	 * @return the Plant on the grid if there is a plant, null otherwise
+	 */
+	public Plant getPlant() {
+		return plant;
+	}
+	
+	/*
+	 * Adds a zombie to the linked list of zombies for that grid cell
+	 */
+	public void addZombie(Zombies zombie) {
+		zombies.add(zombie);
 	}
 	
 	/* 
-	 * Adds a new visitor to the current grid 
+	 * Adds a new plant if the grid doesn't have a current plant
 	 **/
-	public void addVisitor(Object entity){
-		if (entity instanceof Plant && !(visitors.get(0) instanceof Plant)) {
-			visitors.addFirst(entity);
+	public void addPlant(Object plant){
+		if(plant != null){
+			this.plant = (Plant) plant;
 		}
-		else if (entity instanceof Zombie){
-			visitors.add(entity);
-		}
-		else {
-			System.out.println("Already a plant in this grid cell.");
-		}
-		visitors.add(entity);
+	}
+	/*
+	 * Removes and returns the plant on the current grid cell 
+	 * @return the plant that was removed
+	 */
+	public Plant removePlant() {
+		temp = plant;
+		plant = null;
+		return temp;
 	}
 	
-	public void removePlant() {
-		visitors.removeFirst();
+	/*
+	 * Removes and returns the zombie if it needs to move to a new grid cell
+	 * @return the zombie that needs to be returned
+	 */
+	public Zombie removeZombie() {
+		Zombie zomb = zombies.removeFirst();
+		return zomb;
 	}
 	
-	public void removeZombie(Zombie type) {
-		visitors.removeFirst();
+	/**
+	 * Removes the zombie at the beginning of the list as if it died
+	 */
+	public void removeAndKillZombie() {
+		zombies.removeFirst();
 	}
-	
-	
 }
