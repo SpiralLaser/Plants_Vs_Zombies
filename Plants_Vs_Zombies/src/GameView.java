245d5d193e77dc8a24c003f5ba732f1a076590c4
Leo <omarleopaz@hotmail.com>
2018-11-16 19:37:50 -0500

    touch ups

--- Plants_Vs_Zombies/src/GameView.java
+++ Plants_Vs_Zombies/src/GameView.java
@@ -130,6 +130,8 @@ public class GameView implements PvZListener{
        plantPanel.setLayout(new GridLayout(1, 2));
        sunPlant = new JButton("S");
        peaPlant = new JButton("P");
+       sunPlant.addActionListener(new PvZController(model, 0, 0));
+       peaPlant.addActionListener(new PvZController(model, 0, 0));
        plantPanel.add(sunPlant);
        plantPanel.add(peaPlant);
        
@@ -138,7 +140,8 @@ public class GameView implements PvZListener{
        buttonPanel.setLayout(new GridLayout(1, 2));
        
        buttonPanel.add(plantPanel);
-       nextTurnButton = new JButton(" ");
+       nextTurnButton = new JButton("End Turn");
+       nextTurnButton.addActionListener(new PvZController(model, 0, 0));
        buttonPanel.add(nextTurnButton);
        
        contentPane.add(buttonPanel);
