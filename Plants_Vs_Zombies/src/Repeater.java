0cade8ee10f989affe895b67b036588cd3868d23
Kevin Sun <kevin-sun9@hotmail.com>
2018-11-20 19:21:12 -0500

    Pushing Master

---
+++ Plants_Vs_Zombies/src/Repeater.java
@@ -0,0 +1,43 @@
+public class Repeater extends Plant {
+	PvZModel game;
+
+	/**
+	 * Constructor 
+	 * @param initialGridCell
+	 * @param newGame
+	 * @author Kevin Sun
+	 */
+	public Repeater(GridCell initialGridCell, PvZModel newGame) {
+
+		super(initialGridCell, newGame);
+        gridCell = initialGridCell;
+		game = newGame;
+	}
+	
+    /**
+     * Returns the identification of being a Repeater
+     */
+    public String getID()
+    {
+        return "R";
+    }
+
+    /**
+     * End of turn actions for this Repeater is shooting the first zombie in the row and does 2 damage.
+     */
+    public void endTurn()
+    {
+    	GridCell cell = game.findZombie(this.getGridCell().getRow(), this.getGridCell().getCol());
+    	if (cell == null)
+    	{
+    		//do nothing if there is no zombie on this row
+    	}
+    	else
+    	{
+    		Zombie firstZomb = cell.getZombie();
+    		firstZomb.zombieHit(2);
+    	}
+    	
+    }
+    
+}
